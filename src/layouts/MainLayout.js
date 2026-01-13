e8d212831c536d2877f03e8a8ee77045a8b482ec
Tên Bạn <ntvu171091@gmail.com>
2026-01-13 22:46:02 +0700

    update

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -5,11 +5,11 @@ import MainHeader from "./MainHeader";
 
 function MainLayout() {
   return (
-    <Grid container justifyContent="center">
+    <Grid container justifyContent="center" m={3}>
       <Grid item xs={12}>
         <MainHeader />
       </Grid>
-      <Grid item xs={10} mt={5}>
+      <Grid item xs={10}>
         <Outlet />
       </Grid>
       <Grid item xs={12}>
